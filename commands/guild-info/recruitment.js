4f4fc9f2e38e084579bce525eca3994bae1c5194
Ryan Wong <me@ryanwong.uk>
2021-06-27 02:51:53 +0100

    Implemented Update Recruitment

--- commands/guild-info/recruitment.js
+++ commands/guild-info/recruitment.js
@@ -6,37 +6,52 @@ const Constants = require('../../constants');
 
 const keyv = new Keyv('sqlite://../../database.sqlite3');
 
+const getEmbed = () => new discord.MessageEmbed()
+  .setTitle("📝 Recruitment")
+  .addFields(
+    { 
+      name: 'What we expect from you',  
+      value : `
+        - You have to be mature, friendly and skilled. 
+        - You have to actively attend the raids every week, being reliable and stable, as well as on time. 
+        - We expect you to come prepared for raids, and to know your class as well as tactics. 
+        - You also have to be able to take criticism.
+      `
+    },
+    { 
+      name: 'What do we offer',  
+      value : `
+        - A 2 raid days a week schedule with the goal to get Cutting Edge each tier
+        - A stable core team with numerous years of mythic experience
+        - A fun and friendly atmosphere
+      `
+    },
+    { 
+      name: '\u200b',  
+      value : 'We are always recruiting players who share a similar approach to raiding as we do.'
+    },
+    { 
+      name: '\u200b',  
+      value : 'Contact @Abradix#7061, @Eclipse#2819, <@105035733558890496> or @Warzania#9808 if you have any questions.'
+    },
+  )
+  .setColor("GREEN");
+
+const getButton = (applicationChannelUrl) => {
+  const buttonRow = new MessageActionRow();
+    let button = new MessageButton()
+      .setLabel("Apply here")
+      .setStyle("url")
+      .setURL(applicationChannelUrl);
+
+    buttonRow.addComponent(button);
+
+  return buttonRow;
+}
+
+
 const createRecruitmentPost = async(msg) => {
-  const embed = new discord.MessageEmbed()
-    .setTitle("📝 Recruitment")
-    .addFields(
-      { 
-        name: 'What we expect from you',  
-        value : `
-          - You have to be mature, friendly and skilled. 
-          - You have to actively attend the raids every week, being reliable and stable, as well as on time. 
-          - We expect you to come prepared for raids, and to know your class as well as tactics. 
-          - You also have to be able to take criticism.
-        `
-      },
-      { 
-        name: 'What do we offer',  
-        value : `
-          - A 2 raid days a week schedule with the goal to get Cutting Edge each tier
-          - A stable core team with numerous years of mythic experience
-          - A fun and friendly atmosphere
-        `
-      },
-      { 
-        name: '\u200b',  
-        value : 'We are always recruiting players who share a similar approach to raiding as we do.'
-      },
-      { 
-        name: '\u200b',  
-        value : 'Contact @Abradix#7061, @Eclipse#2819, <@105035733558890496> or @Warzania#9808 if you have any questions.'
-      },
-    )
-    .setColor("GREEN");
+  const embed = getEmbed();
 
   let applicationChannelUrl = await keyv.get(Constants.APPLICATION_CHANNEL_URL)
 
@@ -44,16 +59,9 @@ const createRecruitmentPost = async(msg) => {
     embed: embed
   };
 
-
   if (applicationChannelUrl)
   {
-    const buttonRow = new MessageActionRow();
-    let button = new MessageButton()
-      .setLabel("Apply here")
-      .setStyle("url")
-      .setURL(applicationChannelUrl);
-
-    buttonRow.addComponent(button);
+    const buttonRow = getButton(applicationChannelUrl);
 
     messageObject.component = buttonRow;
   } 
@@ -62,7 +70,45 @@ const createRecruitmentPost = async(msg) => {
     msg.reply("Application Channel Url not set, so no button will appear for recruitment.");
   }
   var postedMessage = await msg.channel.send('', messageObject);
+
+  await keyv.set(Constants.GUILDINFO_RECRUITMENT_POST, {
+    channelId: postedMessage.channel.id,
+    postId: postedMessage.id
+  });
+
   return postedMessage;
+};
+
+const updateRecruitmentPost = async(msg) => {
+  const embed = getEmbed();
+
+  var recruitmentPost = await keyv.get(Constants.GUILDINFO_RECRUITMENT_POST);
+
+  if (!recruitmentPost) return msg.reply("Recruitment Post does currently not exist");
+
+  let applicationChannelUrl = await keyv.get(Constants.APPLICATION_CHANNEL_URL)
+
+  let messageObject = {
+    embed: embed
+  };
+
+  if (applicationChannelUrl)
+  {
+    const buttonRow = getButton(applicationChannelUrl);
+
+    messageObject.component = buttonRow;
+  } 
+  else 
+  {
+    msg.reply("Application Channel Url not set, so no button will appear for recruitment.");
+  }
+
+  msg.client.channels.fetch(recruitmentPost.channelId)
+    .then(channel => channel.messages.fetch(recruitmentPost.postId)
+      .then(message => message.edit('', messageObject))
+      .catch(console.error)
+    )
+    .catch(console.error);
 };
 
 module.exports = class RecruitmentCommand extends commando.Command {
@@ -101,6 +147,7 @@ module.exports = class RecruitmentCommand extends commando.Command {
         await createRecruitmentPost(msg);
         break;
       case 'update':
+        await updateRecruitmentPost(msg);
         break;
       case 'delete':
         break;
