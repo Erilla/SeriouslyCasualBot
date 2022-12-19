a197ff1626bf4c1518b2989488b4d034c8f7e830
Ryan Wong <me@ryanwong.uk>
2022-12-19 17:53:33 +0000

    Now pinning trial logs

--- functions/trial-review/updateTrialLogsContent.js
+++ functions/trial-review/updateTrialLogsContent.js
@@ -4,23 +4,24 @@ const Keyv = require('keyv');
 const { generateTrialLogsContent } = require('./generateTrialLogsContent');
 
 const trials = new Keyv(databaseString, { namespace: 'trials' });
-trials.on('error', err => console.error('Keyv connection error:', err));
+trials.on('error', (err) => console.error('Keyv connection error:', err));
 
 async function updateTrialLogsContent(client, trial) {
-
 	const trialReviewChannel = await client.channels
 		.fetch(trialReviewChannelId)
-		.catch(err => console.error(err));
+		.catch((err) => console.error(err));
 	const trialReviewMessage = await trialReviewChannel.messages
 		.fetch(trial.trialReviewId)
-		.catch(err => console.error(err));
+		.catch((err) => console.error(err));
 	if (trial.trialLogsId) {
 		const trialLogsMessage = await trialReviewMessage.thread.messages
 			.fetch(trial.trialLogsId)
-			.catch(err => console.error(err));
+			.catch((err) => console.error(err));
 		await trialLogsMessage
 			.edit(await generateTrialLogsContent(trial))
-			.catch(err => console.error(err));
+			.catch((err) => console.error(err));
+
+		await trialLogsMessage.pin();
 		console.log(`Updated Trial Log Message for ${trial.characterName}`);
 	}
 	else {
